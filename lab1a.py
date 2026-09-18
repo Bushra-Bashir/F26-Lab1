776071eb08b857bba97a513eaf52e4d1dbb7bc2a
Bushra-Bashir <86313770+Bushra-Bashir@users.noreply.github.com>
2026-09-18 16:24:52 +0000

    Completed Lab1a in class

--- lab1a.py
+++ lab1a.py
@@ -1,18 +1,20 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author:Bushra Bashir
+# Date: 18/9/2026
 # Purpose: Create a variable, check its type and print the variable.
 # Usage: python3 lab1a.py
-
 message="Welcome to PRG101"
 print(message)
 print(type(message))
+newsrting=message+"Python is future"
+#print(message+"Python is future")
+print(newsrting)
 x=10
-print(type(x))
-x="Python is future"
 print(type(x))
 """" This is a multi 
 line comment"""
+x="hello"
+print(type(x))
 #I observed that type of x changed from int to str due to python dynamic typing.
